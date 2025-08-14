aec603aa001698bb700714a36de6d0e8cfc19245
dumoedas <schinemann.eduardo@escola.pr.gov.br>
2025-08-14 08:58:07 -0300

    lll

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Futebol',
+    'Memphis e a maior contratação do futebol brasileiro?',
+    'SIM!!!!!!!!! Sem sombra de duvidas'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual a torcida mais modinha do brasil?',
+    'Flamengo/Palmeiras duas torcidas ridiculas em um nivel absurdo'
+)
+
+criaCartao(
+    'Futebol',
+    'Papai cris ou Leo Messi?',
+    'Messi.'
+)
+    criaCartao(
+    'Futebol',
+    'Zacka de lateral ou zagueiro?',
+    'é originalmente zagueiro, mas ao longo da carreira também foi utilizado como lateral em algumas situações, especialmente em esquemas com três defensores. Vou assumir que estamos falando do Zaca conhecido por jogar no CCM e no UPH  e outros clubes brasileiros.'
+)
